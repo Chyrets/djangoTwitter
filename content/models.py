b64b90d1aaec7f515aea5bf720dc86f05185fadc
chyrets <uladzislawchyrets@gmail.com>
2021-07-02 12:58:57 +0300

    Добавил модель поста

--- content/models.py
+++ content/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    """Модель поста"""
+    text = models.TextField(verbose_name='Текст поста', max_length=256)
+    date = models.DateTimeField(verbose_name='Дата публикации', auto_now_add=True)
+    author = models.ForeignKey(User, verbose_name='Автор поста', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
